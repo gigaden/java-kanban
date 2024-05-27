a138e9e5f88ed0015b901bcf77c3e1beff0d1b20
gigaden <gigaden@ya.ru>
2024-05-27 12:58:24 +0300

    fixed codestyle

--- src/ru/yandex/todo/service/FileBackedTaskManager.java
+++ src/ru/yandex/todo/service/FileBackedTaskManager.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
